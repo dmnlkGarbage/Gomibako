400292c4783cf5b582dad970662f5f06eca61cc3
dmnlk <seikima2demon@gmail.com>
2014-12-20 23:44:33 +0900

    add method, pointer val

--- src/http.go
+++ src/http.go
@@ -6,6 +6,32 @@ import (
 
 func main() {
 	var myApp = App{2, "aaaa"}
-	fmt.Printf("%+v", myApp)
-	fmt.Println("aaaaaa")
+	fmt.Println(myApp.appId)
+
+	//このスコープのmyAppの中身は変えない
+	myApp.changeAppId(13)
+	fmt.Println(myApp.appId)
+
+
+	//中身を変更できる
+	myApp.changeAppIdPointer(15)
+	fmt.Println(myApp.appId)
+
+}
+
+
+// scan.goに定義されてるAppっていう構造体にメソッドを生やす。
+
+
+// appのidを値渡しで変える。インスタンス自体の中身は変えない
+func (a App) changeAppId(id int) {
+	a.appId = id
+	fmt.Println(a.appId)
+}
+
+
+// appのidを参照渡しで変える。インスタンス自体の中身を変更できる。ポインタ使ってる
+func (a *App) changeAppIdPointer(id int) {
+	a.appId = id
+	fmt.Println(a.appId)
 }
