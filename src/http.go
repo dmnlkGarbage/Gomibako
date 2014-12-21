c4da2c3e52828c1b0ed93f5b7a3e1aaf34fc26ed
dmnlk <seikima2demon@gmail.com>
2014-12-22 00:35:12 +0900

    add comment

--- src/http.go
+++ src/http.go
@@ -4,6 +4,8 @@ import (
 	"net/url"
 	"net/http"
 	"fmt"
+//	"github.com/k0kubun/pp"
+	"io/ioutil"
 	"github.com/k0kubun/pp"
 )
 
@@ -11,18 +13,27 @@ var URL = "http://api.atnd.org/events/"
 
 
 func main() {
+
+	//パラメータの組み立て
 	values := url.Values{}
 	values.Add("keyword", "golang")
 	values.Add("format", "json")
-	fmt.Println(URL + "?" + values.Encode())
+
+	// Encodeすると?hoge=fuga&piyo=poyo みたいにしてくれる
+	// getRequest送る
 	resp, err := http.Get(URL + "?" + values.Encode())
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
+	// ちゃんとclose
 	defer resp.Body.Close()
-	pp.Print(resp)
-	fmt.Printf("%+v", resp.Body)
+	// レスポンスヘッダしか読めない？
+	fmt.Println(resp)
+
+	// ioutil.readallでバイト配列にする
+	val, err := ioutil.ReadAll(resp.Body)
+	// バイト配列を文字列にして表示する
+	pp.Print(string(val))
 
 }
