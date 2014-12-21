c5173bc703569d44a1b1debbc20ad771115ba0a5
dmnlk <seikima2demon@gmail.com>
2014-12-22 00:19:47 +0900

    test atnd api

--- src/http.go
+++ src/http.go
@@ -1,2 +1,28 @@
-package src
+package main
 
+import (
+	"net/url"
+	"net/http"
+	"fmt"
+	"github.com/k0kubun/pp"
+)
+
+var URL = "http://api.atnd.org/events/"
+
+
+func main() {
+	values := url.Values{}
+	values.Add("keyword", "golang")
+	values.Add("format", "json")
+	fmt.Println(URL + "?" + values.Encode())
+	resp, err := http.Get(URL + "?" + values.Encode())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+	pp.Print(resp)
+	fmt.Printf("%+v", resp.Body)
+
+}
