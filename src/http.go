0f4d051dd10a130c3164bda49b72042351da0793
dmnlk <seikima2demon@gmail.com>
2014-12-22 00:45:00 +0900

    use httpClient

--- src/http.go
+++ src/http.go
@@ -4,7 +4,6 @@ import (
 	"net/url"
 	"net/http"
 	"fmt"
-//	"github.com/k0kubun/pp"
 	"io/ioutil"
 	"github.com/k0kubun/pp"
 )
@@ -13,6 +12,8 @@ var URL = "http://api.atnd.org/events/"
 
 
 func main() {
+
+	//普通にhttpパッケージ使ってみる
 
 	//パラメータの組み立て
 	values := url.Values{}
@@ -35,5 +36,26 @@ func main() {
 	val, err := ioutil.ReadAll(resp.Body)
 	// バイト配列を文字列にして表示する
 	pp.Print(string(val))
+
+
+
+	// httpClient使ってみる
+	req, nil := http.NewRequest("GET", URL, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	client := new(http.Client)
+	resp1, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp1.Body.Close()
+	fmt.Println(resp1)
+	// 取れる中身全然違うっぽい？
+	val1, err := ioutil.ReadAll(resp1.Body)
+	pp.Print(val1)
 
 }
