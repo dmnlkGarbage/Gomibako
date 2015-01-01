71c3d3153cc504e9f11a950cd51a0a2a146d718a
dmnlk <seikima2demon@gmail.com>
2015-01-01 19:13:15 +0900

    write comment

--- src/oauth_sample.go
+++ src/oauth_sample.go
@@ -9,6 +9,7 @@ import (
 	"flag"
 )
 
+//OauthClient
 type Client struct {
 	consumer *oauth.Consumer
 	accessToken *oauth.AccessToken
@@ -23,8 +24,10 @@ func main() {
 	pp.Print(result)
 }
 
-
+// client初期化
 func initialize() *Client {
+
+	//それぞれのkeyを実行時引数から取得
 	var consumerKey *string = flag.String(
 		"consumerkey",
 		"",
@@ -44,34 +47,47 @@ func initialize() *Client {
 		"accesssecret",
 		"",
 		"Consumer Secret from Twitter. See: https://dev.twitter.com/apps/new")
+
+	//parseしないと取れない
 	flag.Parse()
 	fmt.Println(*consumerKey)
 	fmt.Println(*consumerSecret)
 	fmt.Println(*accessToken)
 	fmt.Println(*accessTokenSecret)
+
+
+	//初期化
 	client := new(Client)
+	//oauthパッケージでtwitterにconsumerkeyを元に取得
 	client.consumer = oauth.NewConsumer(*consumerKey, *consumerSecret, oauth.ServiceProvider{
 			RequestTokenUrl:   "http://api.twitter.com/oauth/request_token",
 			AuthorizeTokenUrl: "https://api.twitter.com/oauth/authorize",
 			AccessTokenUrl:    "https://api.twitter.com/oauth/access_token",
 		})
+	//accessTokenを渡す。
+	//ユーザーに認証させることもできるが面倒なので
 	client.accessToken = &oauth.AccessToken{*accessToken, *accessTokenSecret, nil}
 	return client
 }
 
+// clientを使ってTwitterからデータ取得する
 func (client *Client) Get(url string, params map[string]string) (interface {}, error) {
+	//Twitterにget
 	response, err := client.consumer.Get(url, params, client.accessToken)
 	if err != nil {
 		fmt.Errorf("error")
 		return nil, err
 	}
-
+	//close大事
 	defer response.Body.Close()
+
+	//jsonっぽいものにする
 	bin, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		fmt.Errorf("error")
-	return nil, err
+		return nil, err
 	}
+	//unMarshalして返す。interfaceで受けるとstruct定義しなくていいのでちょっと楽
 	var result interface{}
 	err = json.Unmarshal(bin, &result)
 	return result, err
