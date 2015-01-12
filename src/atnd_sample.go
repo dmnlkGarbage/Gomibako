2624ed083e76dee1e0778e491acf9acbfeb1a545
dmnlk <seikima2demon@gmail.com>
2015-01-13 01:12:57 +0900

    parse json to interface

---
+++ src/atnd_sample.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/url"
+	"net/http"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/k0kubun/pp"
+)
+
+const (
+	URL = "http://api.atnd.org/events/"
+)
+func main() {
+	param := url.Values{}
+	param.Add("keyword", "golang")
+	param.Add("format", "json")
+
+	resp, err := http.Get(URL + "?" + param.Encode())
+	if err != nil {
+		fmt.Println(err)
+		return;
+	}
+	defer resp.Body.Close()
+	val, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return;
+	}
+
+	var result interface{}
+	if err = json.Unmarshal(val, &result);err != nil {
+		fmt.Println(err)
+		return
+	}
+	pp.Print(result)
+
+}
+
