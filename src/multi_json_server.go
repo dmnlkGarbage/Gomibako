9092cd9ece7ec2805a801ef69874d688913d0513
dmnlk <seikima2demon@gmail.com>
2015-01-28 01:57:52 +0900

    add mulit json by rand

--- src/multi_json_server.go
+++ src/multi_json_server.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"time"
 	"fmt"
+	"math/rand"
+	"encoding/json"
 )
 
 
@@ -16,10 +18,33 @@ func helloStreaming(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(200)
 
 	for  {
-		w.Write([]byte("hello\n"))
+		rand.Seed(time.Now().Unix())
+		rand := rand.Intn(2)
+		if rand == 0 {
+			u := new(User)
+			u.Name = "a"
+			u.Age = 22
+			enc, _ := json.Marshal(u)
+			w.Write(enc)
+		} else {
+			e := new(Event)
+			e.Title = "title"
+			enc, _ := json.Marshal(e)
+			w.Write(enc)
+		}
+
 		w.(http.Flusher).Flush()
 		time.Sleep(1 * time.Second)
 	}
 
 	fmt.Fprintf(w, "hello end\n")
+}
+
+type User struct {
+	Name string
+	Age int64
+}
+
+type Event struct {
+	Title string
 }
