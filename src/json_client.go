6e02649dc46ce5db32ef85e617d0068c690232d4
dmnlk <seikima2demon@gmail.com>
2015-02-05 01:02:14 +0900

    fix #2

--- src/json_client.go
+++ src/json_client.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/k0kubun/pp"
 )
 
 func main() {
@@ -22,12 +24,36 @@ func main() {
 
 		//まずinterfaceでうける
 		//json化
-		var result = interface {}
-		fmt.Println(result)
-		//		if err = json.Unmarshal(scanner.Bytes(), &result); err != nil {
-		//			fmt.Println(err)
-		//			continue
-		//		}
+		var result interface{}
+
+		if err = json.Unmarshal(scanner.Bytes(), &result); err != nil {
+			fmt.Println(err)
+			continue
+		}
+		msg := result.(map[string]interface{})
+		if _, ok := msg["Title"]; ok {
+			u := new(User)
+			if err = json.Unmarshal(scanner.Bytes(), &u); err != nil {
+				break
+			}
+			pp.Print(u)
+		}
+		if _, ok := msg["Name"]; ok  {
+			e := new(Event)
+			if err = json.Unmarshal(scanner.Bytes(), &e); err != nil {
+				break
+			}
+			pp.Print(e)
+		}
 
 	}
+}
+
+type User struct {
+	Name string
+	Age  int64
+}
+
+type Event struct {
+	Title string
 }
---
+++ src/structRepo.go
@@ -0,0 +1,12 @@
+package src
+
+type User struct {
+	Name string
+	Age  int64
+}
+
+type Event struct {
+	Title string
+}
+
+
