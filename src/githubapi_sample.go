972f9be6a7f66427b7139df7cb48e050cf7d91a2
dmnlk <seikima2demon@gmail.com>
2015-02-23 00:01:54 +0900

    gomibako

--- src/githubapi_sample.go
+++ src/githubapi_sample.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/google/go-github/github"
+import (
+	"github.com/google/go-github/github"
+	"net/http"
+)
 
 func main() {
-	c := github.NewClient()
+	c := github.NewClient(http.Client{})
+	if c == nil {
+		return;
+	}
 }
 
---
+++ src/prowl.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/rem7/goprowl"
+	"fmt"
+	"os"
+)
+
+
+func main() {
+	key := os.Getenv("prowl_key")
+	var p goprowl.Goprowl
+	err := p.RegisterKey(key)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	n := &goprowl.Notification{
+		Application : "Foo",
+		Description: "Foobar!",
+		Event : "Bar",
+		Priority : "1",
+		Providerkey : "",
+		Url: "www.foobar.com",
+	}
+	p.Push(n)
+}
+
