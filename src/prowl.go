0c2720149eb53699352b2c23c846c26ac977e6df
dmnlk <seikima2demon@gmail.com>
2015-02-24 01:40:20 +0900

    fix val

--- src/prowl.go
+++ src/prowl.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"github.com/rem7/goprowl"
 	"fmt"
 	"os"
-)
 
+	"github.com/rem7/goprowl"
+	"strconv"
+)
 
 func main() {
 	key := os.Getenv("prowl_key")
@@ -17,20 +18,26 @@ func main() {
 	}
 
 	n := &goprowl.Notification{
-		Application : "Foo",
+		Application: "Foo",
 		Description: "Foobar!",
-		Event : "Bar",
-		Priority : "1",
-		Providerkey : "",
-		Url: "www.foobar.com",
+		Event:       "Bar",
+		Priority:    "1",
+		Providerkey: "",
+		Url:         "www.foobar.com",
 	}
 
-	go func () {
-		err = p.Push(n)
-	}()
+	c := make(chan int)
+	for i := 1; i < 5; i++ {
+		fmt.Println(i)
+		n.Description = "test" + strconv.Itoa(i)
+		go func() {
+			err = p.Push(n)
+			c <- 1
+		}()
+	}
 
+	fmt.Println(<-c)
 	if err != nil {
 		fmt.Println("Unable to send Prowl notification! - " + err.Error())
 	}
 }
-
