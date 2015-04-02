1aaba2d9e9ca494ef8616b8ac4a93004a47fad30
dmnlk <seikima2demon@gmail.com>
2015-04-03 01:05:08 +0900

    template

--- src/htmltemplate.go
+++ src/htmltemplate.go
@@ -1,5 +1,30 @@
-package src
+package main
+
+import (
+	"text/template"
+	"os"
+	"fmt"
+)
+
+type Member struct {
+	id   int
+	Name string
+	Tech string
+}
 
 func main() {
-	
+	// template用
+	const template_text = `template name is {{.Name}}`
+
+	// Paraseする
+	tpl := template.Must(template.New("mytemp").Parse(template_text))
+
+	// 適当な構造体
+	member := Member{1,"dmnlk", "Java"}
+
+	// 埋め込んで標準出力
+	if err := tpl.Execute(os.Stdout, member);err != nil {
+		fmt.Println(err)
+		return
+	}
 }
